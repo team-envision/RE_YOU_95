613bde32d65f112d54ad0783311d87afd727c55b
AdixSasuke <adityakhandelwal0271@gmail.com>
2024-10-16 00:24:49 +0530

    aboutus

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 
 import HurryUp from "./Hurryup";
 import Footer from "./Footer";
+import AboutUs from "./AboutUs";
 export default function Home() {
   return (
     <>
-      
+      <AboutUs/>
       <HurryUp/>
       <Footer/>
     

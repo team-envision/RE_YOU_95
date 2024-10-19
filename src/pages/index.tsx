9ba561f5bcbf42b25763047bad591d2865dd52e6
AdixSasuke <138802256+AdixSasuke@users.noreply.github.com>
2024-10-19 18:56:20 +0530

    Revert "aboutus"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
 
 import HurryUp from "./Hurryup";
 import Footer from "./Footer";
-import AboutUs from "./AboutUs";
 export default function Home() {
   return (
     <>
-      <AboutUs/>
+      
       <HurryUp/>
       <Footer/>
     
